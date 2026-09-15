8168ebbabe735b741db88cb3db6a906fd3a96bae
Simon <simonuwusierra@gmail.com>
2026-09-15 12:26:41 -0500

    Revert "Se implemento el funcionamiento en la pagina de admin donde se administran los usuarios de la pagina"
    This reverts commit 1fc5c8900ca53678a88dcecda93221a7136ae743.

--- backend/lib/auth.js
+++ backend/lib/auth.js
@@ -95,14 +95,6 @@ function deleteSession(sid) {
   sessions.delete(sid);
 }
 
-function deleteSessionsForUser(userId) {
-  for (const [sid, session] of sessions.entries()) {
-    if (String(session.userId) === String(userId)) {
-      sessions.delete(sid);
-    }
-  }
-}
-
 function generateAccessToken(payload) {
   return jwt.sign(payload, JWT_SECRET, { expiresIn: '15m' });
 }
@@ -237,5 +229,4 @@ module.exports = {
   createSession,
   getSession,
   deleteSession,
-  deleteSessionsForUser,
 };
